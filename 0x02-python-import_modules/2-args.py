b7c06c904802ad83a5aa86bbe95a869ce5b39600
Obi <williamobi818@gmail.com>
2023-06-08 16:04:17 +0000

    energy

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    a = len(sys.argv) - 1
+    from sys import argv
+    a = len(argv) - 1
     if a == 1:
         print("{} argument".format(a))
-        print("{}: {}".format(a, sys.argv[1]))
+        print("{}: {}".format(a, argv[1]))
     else:
         print("{} arguments".format(a))
         if a > 0:
             for c in range(a):
-                print("{}: {}".format(c + 1, sys.argv[c + 1]))
+                print("{}: {}".format(c + 1, argv[c + 1]))
