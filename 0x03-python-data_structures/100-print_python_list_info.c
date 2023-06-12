be8b15dcc8d240a9ffcaa045466f1ce69c2227fc
Obi <williamobi818@gmail.com>
2023-06-12 21:27:54 +0000

    work

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,35 +1,26 @@
-#include <stdio.h>
 #include <Python.h>
-/**
- * print_python_list_info - Prints basic info about Python lists
- * @p: A PyObject list
- */
 
+/**
+ * print_python_list_info - Prints basic info about Python lists.
+ * @p: A PyObject list.
+ */
 void print_python_list_info(PyObject *p)
 {
-	Py_ssize_t size, i;
+	int size, alloc, i;
 	PyObject *obj;
 
-	size = PyList_Size(p);
-	if (size == -1)
-	{
-		printf("[*] Error: Invalid Python list.\n");
-		return;
-	}
-	printf("[*] Size of the Python List: %ld\n", size);
-	printf("[*] Allocated Memory: %ld\n", ((PyListObject *)p)->allocated);
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
 
 	for (i = 0; i < size; i++)
 	{
+		printf("Element %d: ", i);
+
 		obj = PyList_GetItem(p, i);
-		if (obj != NULL)
-		{
-			printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
-		}
-		else
-		{
-			printf("Element %ld: [Error retrieving object]\n", i);
-		}
+		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
 
