37fbdc08c51c0f43aa901c4403e2241de98c254a
Obi <williamobi818@gmail.com>
2023-06-12 16:37:55 +0000

    gits

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return None
-    elif idx > (len(my_list)):
-        return my_list
-    else:
+    if idx >= 0 and idx < len(my_list):
         b_list = my_list.copy()
         b_list[idx] = element
         return b_list
+    else:
+        return my_list
