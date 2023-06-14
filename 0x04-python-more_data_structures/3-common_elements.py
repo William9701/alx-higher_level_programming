ddb6876263374605ad0e51bdfd291b9c8dbccf2e
Obi <williamobi818@gmail.com>
2023-06-14 12:51:01 +0000

    m

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     if len(set_1) != 0 and len(set_2) != 0:
-        can = []
-        for x in set_1:
-            for y in set_2:
-                if x == y:
-                    can.append(x)
-
-        return can
+        return (set_1 & set_2)
     else:
         return None
