0ad76b273bf46af1575adc4700c9e86756de66c8
Obi <williamobi818@gmail.com>
2023-06-15 21:07:47 +0000

    fix

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,5 +3,9 @@ def only_diff_elements(set_1, set_2):
     if len(set_1) != 0 and len(set_2) != 0:
         c = set_1 ^ set_2
         return c
+    elif len(set_1) == 0:
+        return list(set_2)
+    elif len(set_2) == 0:
+        return list(set_1)
     else:
         return ()
