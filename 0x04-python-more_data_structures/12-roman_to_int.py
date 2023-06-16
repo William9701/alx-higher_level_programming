9293e45946fc7296c2e4668298d14376781c5450
Obi <williamobi818@gmail.com>
2023-06-16 21:30:39 +0000

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    fig = 0
+
+    for i in range(len(roman_string)):
+        # Check if the next numeral has a greater value
+        if i + 1 < len(roman_string) and roman[roman_string[i]] < roman[roman_string[i + 1]]:
+            fig -= roman[roman_string[i]]
+        else:
+            fig += roman[roman_string[i]]
+
+    return fig
