49fe1891945461cadb491bff2c344e3947cf810b
Obi <williamobi818@gmail.com>
2023-06-16 22:59:07 +0000

    map

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    new = map(lambda x: list(map(lambda y: y**2, x)), matrix)
-    return list(new)
+    if len(matrix) != 0:
+        return (list(map(lambda x: list(map(lambda y: y**2, x)), matrix)))
+    return 0
