4b2c98670c34387141fbbe22968d30e43f089581
Obi <williamobi818@gmail.com>
2023-06-26 15:05:31 +0000

    more work

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    try:
+        for item in range(x):
+            if isinstance(my_list[item], int):
+                print("{:d}".format(my_list[item]), end='')
+                count += 1
+                if count == x:
+                    break
+        print()
+    except TypeError:
+        pass
+
+    return count
