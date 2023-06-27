c1ea4b2580fc37ec9215901b5ff399d37deeebc7
Obi <williamobi818@gmail.com>
2023-06-27 15:05:25 +0000

    work

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,8 +3,4 @@ class Square:
     '''
     This class specifies the area and everything that has to do with the square
     '''
-    def __int__(self):
-        '''
-        this is the init defininition still defining it self
-        '''
-        self.square = 0
+    pass
