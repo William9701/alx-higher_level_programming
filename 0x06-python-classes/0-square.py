79b13897b91e4d33abf36d6d2ce76ea08fe8c955
Obi <williamobi818@gmail.com>
2023-06-27 15:02:43 +0000

    work

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    '''
+    This class specifies the area and everything that has to do with the square
+    '''
+    def __int__(self):
+        '''
+        this is the init defininition still defining it self
+        '''
+        self.square = 0
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,1 @@
+this repo contains class and objects in python
