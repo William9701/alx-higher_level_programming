ab2b177a1c2ee9a7cf456cbd1ee0853a1cf5f176
Obi <williamobi818@gmail.com>
2023-09-22 05:57:30 +0000

    qa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,8 +2,8 @@
 """Start link class to table in database 
 """
 import sys
-from model_state import State
-from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
