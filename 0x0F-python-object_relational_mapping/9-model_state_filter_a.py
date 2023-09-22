3a6242ad08a0d2dcc1dcace338f65d10a8fe106a
Obi <williamobi818@gmail.com>
2023-09-22 10:51:36 +0000

    trial 11

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,16 +1,35 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
+"""Script to query the database and display State objects that contain the
+letter 'a' in their name. Usage: python script.py <mysql_username>
+<mysql_password> <database_name>"""
+
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-	Session = sessionmaker(bind=engine)
-	session = Session()
+    # Check for the correct number of arguments
+    if len(sys.argv) != 4:
+        print(
+            "Usage: python script.py <mysql_username> <mysql_password> "
+            "<database_name>")
+        sys.exit(1)
 
-	for state in session.query(State).order_by(State.id):
-		if "a" in state.name:
-			print("{}: {}".format(state.id, state.name))
+    # Create the SQLAlchemy engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2],
+                                                    sys.argv[3]),
+        pool_pre_ping=True)
+
+    # Bind the Base class to the engine
+    Base.metadata.bind = engine
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query for State objects and print those with 'a' in their name
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
