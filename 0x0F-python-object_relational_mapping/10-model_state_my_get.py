b3c323ae0dccfaacc55d2853a37e353a05bfc074
Obi <williamobi818@gmail.com>
2023-09-22 10:55:27 +0000

    trial 12

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
+"""Script to query the database and find the ID of a State by its name.
+Usage: python script.py <mysql_username> <mysql_password> <database_name>
+<state_name>"""
+
 import sys
 from model_state import Base, State
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    # Check for the correct number of arguments
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <mysql_username> <mysql_password> "
+              "<database_name> <state_name>")
+        sys.exit(1)
+
+    # Create the SQLAlchemy engine
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2],
                                                     sys.argv[3]),
         pool_pre_ping=True)
+
+    # Bind the Base class to the engine
+    Base.metadata.bind = engine
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -20,5 +34,6 @@ if __name__ == "__main__":
             print("{}".format(state.id))
             found = True
             break
-    if found == False:
+
+    if not found:
         print("Not found")
