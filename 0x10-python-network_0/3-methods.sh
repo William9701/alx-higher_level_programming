c08958d312ea805dcd2fcc3708e5a89d6ee797c9
Obi <williamobi818@gmail.com>
2023-09-28 22:54:44 +0000

    tyr

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -X OPTIONS -i -sS $1 | grep -i "Allow:" | sed -e 's/Allow: //I'
+curl -X OPTIONS -i -sS "$1" | grep -i "Allow:" | sed -e 's/Allow: //I'
