dc9ebdb6161caaf11cdf1cff194d45b58bc756e8
Obi <williamobi818@gmail.com>
2023-09-28 22:17:29 +0000

    again

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 # a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-if [ $# -lt 1 ]; then
-	  echo "Usage: $0 <URL>"
-	    exit 1
-fi
-
-URL=$1
-
-# Send an OPTIONS request to the URL to determine accepted HTTP methods
-curl -X OPTIONS -i -sS $URL | grep -i "Allow:" | sed -e 's/Allow: //I'
+curl -X OPTIONS -i -sS $1 | grep -i "Allow:" | sed -e 's/Allow: //I'
 
