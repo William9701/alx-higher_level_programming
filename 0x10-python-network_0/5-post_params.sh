a4c2b565ef9008c789f5639fdede83984ca54bed
Obi <williamobi818@gmail.com>
2023-09-28 23:27:11 +0000

    h

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" -i -sS "$1" | sed -n '/^\s*$/,$p'
+curl -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" -i -sS "$1" | sed -n '/^\s*$/,$p'
