0f3df40fedf6c5ddc50f61028ea10559945536f2
Obi <williamobi818@gmail.com>
2023-09-29 19:36:13 +0000

    on it

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -2,11 +2,12 @@
 """a Python script that takes in a URL, sends a request to the URL and
 displays the value of the variable X-Request-Id in the response header"""
 
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-r = requests.get(sys.argv[1])
-hold = r.headers
-for key, value in hold.items():
-    if key == "X-Request-Id":
-        print(value)
+    r = requests.get(sys.argv[1])
+    hold = r.headers
+    for key, value in hold.items():
+        if key == "X-Request-Id":
+            print(value)
