33fe35a4cec508a0f60ab2608eb0fa368ddd56e7
Obi <williamobi818@gmail.com>
2023-09-29 00:23:37 +0000

    head

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -I -s "$1" | awk '/HTTP/{print $2}'
+curl -o /dev/null -s -w "%{http_code}\n" "$1"
