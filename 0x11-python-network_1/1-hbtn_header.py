996d9eed985eac8ff8afdb1732b7e5345f0866d8
Obi <williamobi818@gmail.com>
2023-09-29 19:21:43 +0000

    high

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,11 +2,12 @@
 """a Python script that takes in a URL, sends a request to the URL and
 displays the value of the X-Request-Id variable found in the header of the
 response."""
-from urllib import request
-import sys
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
-with request.urlopen(sys.argv[1]) as response:
-    html = response.info()
-    for key, value in html.items():
-        if key == "X-Request-Id":
-            print(value)
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        for key, value in html.items():
+            if key == "X-Request-Id":
+                print(value)
