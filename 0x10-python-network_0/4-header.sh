13d6affc91cbb22a2bfc4286bb0807362bc748c9
Obi <williamobi818@gmail.com>
2023-09-29 01:37:59 +0000

    gh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request with the specified header and display the body of the response
-curl -X GET -H "X-School-User-Id: 98" -i -sS "$1" | sed -n '/^\s*$/,$p'
+curl -sH "X-School-User-Id: 98" "$1"
