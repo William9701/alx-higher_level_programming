54b1470a0d6533324365a75cba80f184c00de5ee
Obi <williamobi818@gmail.com>
2023-10-25 02:59:58 +0000

    hy

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,24 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-if (process.argv.length <= 2) {
-  console.log('Please provide the API URL as an argument');
-  process.exit(-1);
-}
-
-const url = process.argv[2];
-let count = 0;
-let movie = '';
-request(url, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const movies = JSON.parse(body);
-    for (movie of movies.results) {
-      if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
-        count += 1;
-      }
-    }
-    console.log(count);
-  } else {
-    console.error(error); // Print the error if one occurred
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
